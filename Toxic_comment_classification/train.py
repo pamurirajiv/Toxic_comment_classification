4cecb7b7d819c5cb32e2c3502a160eb111c2eb45
pamurirajiv <pamurirajivedu03@gmail.com>
2026-04-07 19:09:42 +0530

    Update toxic comment training script

--- Toxic_comment_classification/train.py
+++ Toxic_comment_classification/train.py
@@ -19,11 +19,8 @@ from tensorflow.keras.metrics import Precision, Recall, AUC
 
 
 LOCAL_DIR = os.path.dirname(__file__)
-
 DATA_PATH = os.path.join(LOCAL_DIR, "train (2).csv")
-
 MODEL_PATH = os.path.join(LOCAL_DIR, "model.h5")
-
 VECTORIZER_PATH = os.path.join(LOCAL_DIR, "vectorizer.keras")
 
 
@@ -35,6 +32,7 @@ if "id" in df.columns:
 
 df["comment_text"] = df["comment_text"].apply(clean_text)
 
+
 label_columns = [
     "toxic",
     "severe_toxic",
@@ -45,16 +43,6 @@ label_columns = [
 ]
 
 
-label_counts = df[label_columns].sum()
-total_samples = len(df)
-
-label_counts = train[label_columns].sum()
-total_samples = len(train)
-class_weights = total_samples / (len(label_columns)*label_counts)
-class_weights
-
-
-
 train, test = train_test_split(
     df,
     test_size=0.2,
@@ -62,9 +50,10 @@ train, test = train_test_split(
 )
 
 x_train = train["comment_text"]
-
 y_train = train[label_columns].values
 
+x_test = test["comment_text"]
+y_test = test[label_columns].values
 
 
 MAX_FEATURES = 200000
@@ -84,6 +73,7 @@ vectorizer_model.save(VECTORIZER_PATH)
 
 
 x_train_vectorized = vectorizer(x_train.values)
+x_test_vectorized = vectorizer(x_test.values)
 
 
 model = Sequential()
@@ -91,8 +81,7 @@ model = Sequential()
 model.add(
     Embedding(
         input_dim=MAX_FEATURES,
-        output_dim=128,
-        input_length=SEQ_LENGTH
+        output_dim=128
     )
 )
 
@@ -110,41 +99,36 @@ model.add(Dense(128, activation="relu"))
 
 model.add(Dense(6, activation="sigmoid"))
 
-model.build(input_shape=(None, SEQ_LENGTH))
-
-model.summary()
-
-
 
 model.compile(
     loss='binary_crossentropy',
     optimizer="adam",
     metrics=[
-        Precision(),
-        Recall(),
-        AUC()
+        Precision(name="precision"),
+        Recall(name="recall"),
+        AUC(name="auc")
     ]
 )
 
+
+model.summary()
 
 
 history = model.fit(
     x_train_vectorized,
     y_train,
-    epochs=4,
-    class_weight=class_weights,
+    epochs=8,
     batch_size=32,
     validation_split=0.2,
     verbose=1
 )
 
 
-x_test = vectorizer(test["comment_text"].values)
+loss, precision, recall, auc = model.evaluate(
+    x_test_vectorized,
+    y_test
+)
 
-y_test = test[label_columns].values
-
-print(model.evaluate(x_test, y_test))
-
-
+print(f"Loss: {loss:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, AUC: {auc:.4f}")
 
 model.save(MODEL_PATH)
